0cbc57de2568afdd55a56bb129e6a3e457f0ea9c
Erick Mulindi <37551785+mulindierick@users.noreply.github.com>
2021-09-21 23:33:05 +0300

    merged braches

--- db.js
+++ db.js
@@ -9,9 +9,9 @@ const isProduction = process.env.NODE_ENV === "production";
 // const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
 const pool = new Pool({
-  connectionString: process.env.DB_CONNECTION_STRING,
-  ssl: { rejectUnauthorized: false },
-  // connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  // ssl: !isProduction ? isProduction : { rejectUnauthorized: false },
+  // connectionString: process.env.DB_CONNECTION_STRING,
+  // ssl: { rejectUnauthorized: false },
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: !isProduction ? isProduction : { rejectUnauthorized: false },
 });
 export default pool;

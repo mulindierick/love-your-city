66f471f870aad9aac8ad49293b187807ad17ff0c
Erick Mulindi <37551785+mulindierick@users.noreply.github.com>
2022-06-05 23:19:09 -0400

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -2,6 +2,10 @@ import bcrypt from "bcrypt";
 import { token } from "../token.js";
 import express from "express";
 import pool from "../db.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import nodemailer from "nodemailer";
+dotenv.config();
 
 import { OAuth2Client } from "google-auth-library";
 const CLIENT_ID = process.env.CLIENT_ID;
@@ -35,6 +39,8 @@ router.post("/", async (req, res) => {
   }
 });
 
+// google auth
+
 router.post("/google", async (req, res) => {
   try {
     const { googleToken } = req.body;
@@ -49,12 +55,94 @@ router.post("/google", async (req, res) => {
     ]);
 
     if (user.rows.length === 0) {
-      return res.json({ error: "some account details are not correct" });
+      return res.json({ error: "some account details are incorrect" });
     }
     res.json({
       token: token(user.rows[0]),
       user: { user_id: user.rows[0].user_id },
     });
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+});
+
+// send passowrd reset url
+router.post("/psw-reset-url", async (req, res) => {
+  try {
+    //first check if the user has an account already
+    // const user = await pool.query("SELECT * FROM users WHERE email = $1", [
+    //   req.body.email,
+    // ]);
+    // if (user.rows.length === 0) {
+    //   return res.json({ msg: "user does not exist" });
+    // }
+    // // if user exists, send token to email
+    // let token = jwt.sign(
+    //   user.rows[0],
+    //   process.env.ACCESS_TOKEN + user.rows[0].password,
+    //   { expiresIn: "15m" }
+    // );
+    // let resetUrl = ` http://localhost:5000/${token}/${user.rows[0].user_id}`;
+
+    // send reset url to user email
+    const transporter = nodemailer.createTransport({
+      service: "Gmail",
+      auth: {
+        user: "",
+        pass: "",
+      },
+    });
+
+    const options = {
+      from: "mulindi@coburwas.org",
+      to: "mulindierick@gmail.com",
+      subject: "LYC test",
+      text: "it worked",
+    };
+
+    transporter.sendMail(options, function (error, res) {
+      if(error){
+        console.log(error.message)
+        return;
+      }
+      console.log(res.response)
+    });
+    res.json({msg: "email sent"})
+
+
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+});
+
+// reset password
+router.post("/reset-psw/:userId/:token", async (req, res) => {
+  try {
+    //check if userID is valid and user is there
+    const user = await pool.query(
+      "SELECT * FROM users WHERE users.user_id = $1",
+      [req.params.userId]
+    );
+    if (user.rows.length === 0) {
+      return res.json({ msg: "bad request" });
+    }
+    // check if token is valid
+    let token = jwt.verify(
+      req.params.token,
+      process.env.ACCESS_TOKEN + user.password
+    );
+    // if token is valid, reset password
+
+    await pool
+      .query("UPDATE users SET password=$1 WHERE id=$2", [
+        bcrypt.hash(req.body.password, 10),
+        req.params.userId,
+      ])
+      .then(() => res.send(`password updated`))
+      .catch((error) => {
+        console.error(error);
+        res.status(500).json(error);
+      });
   } catch (error) {
     res.json({ error: error.message });
   }

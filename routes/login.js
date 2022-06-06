a5f5947c2ad1a5950e3c8235cce5d599902b9239
Erick Mulindi <37551785+mulindierick@users.noreply.github.com>
2022-06-06 12:09:57 -0400

    psw-reset complete

--- routes/login.js
+++ routes/login.js
@@ -69,20 +69,22 @@ router.post("/google", async (req, res) => {
 // send passowrd reset url
 router.post("/psw-reset-url", async (req, res) => {
   try {
-    //first check if the user has an account already
-    // const user = await pool.query("SELECT * FROM users WHERE email = $1", [
-    //   req.body.email,
-    // ]);
-    // if (user.rows.length === 0) {
-    //   return res.json({ msg: "user does not exist" });
-    // }
-    // // if user exists, send token to email
-    // let token = jwt.sign(
-    //   user.rows[0],
-    //   process.env.ACCESS_TOKEN + user.rows[0].password,
-    //   { expiresIn: "15m" }
-    // );
-    // let resetUrl = ` http://localhost:5000/${token}/${user.rows[0].user_id}`;
+    // first check if the user has an account already
+    const user = await pool.query("SELECT * FROM users WHERE email = $1", [
+      req.body.email,
+    ]);
+    if (user.rows.length === 0) {
+      return res.json({ msg: "user does not exist" });
+    }
+    
+    // if user exists, send token to email
+    console.log("1", process.env.ACCESS_TOKEN + user.password)
+    let token = jwt.sign(
+      user.rows[0],
+      (process.env.ACCESS_TOKEN + user.rows[0].password),
+      { expiresIn: "15m" }
+    );
+    let resetUrl = `http://localhost:5000/login/reset-psw/${token}/${user.rows[0].user_id}`;
 
     // send reset url to user email
     const transporter = nodemailer.createTransport({
@@ -97,24 +99,26 @@ router.post("/psw-reset-url", async (req, res) => {
       from: process.env.FROM,
       to: process.env.TO_EMAIL,
       subject: "LYC test",
-      text: "it worked",
+      text: `${resetUrl}`,
     };
 
     transporter.sendMail(options, function (error, res) {
       if (error) {
         console.log({ eror: error.message });
+        return res.json({ msg: "email sent" });
       } else {
         console.log(res.response);
         return res.json({ msg: "email sent" });
       }
     });
+    return res.status(500).json({ msg: "success" });
   } catch (error) {
     res.json({ error: error.message });
   }
 });
 
 // reset password
-router.post("/reset-psw/:userId/:token", async (req, res) => {
+router.post("/reset-psw/:token/:userId", async (req, res) => {
   try {
     //check if userID is valid and user is there
     const user = await pool.query(
@@ -124,17 +128,17 @@ router.post("/reset-psw/:userId/:token", async (req, res) => {
     if (user.rows.length === 0) {
       return res.json({ msg: "bad request" });
     }
+    // console.log(user.rows)
+    console.log("2", user.rows[0].password)
     // check if token is valid
-    let token = jwt.verify(
-      req.params.token,
-      process.env.ACCESS_TOKEN + user.password
-    );
-    // if token is valid, reset password
+    console.log("2", process.env.ACCESS_TOKEN + user.password)
+    jwt.verify(req.params.token, (process.env.ACCESS_TOKEN + user.rows[0].password));
 
+
+    // if token is valid, reset password
+    let newPsw = await bcrypt.hash(req.body.password, 10)
     await pool
-      .query("UPDATE users SET password=$1 WHERE id=$2", [
-        bcrypt.hash(req.body.password, 10),
-        req.params.userId,
+      .query("UPDATE users SET password=$1 WHERE user_id=$2", [newPsw, req.params.userId,
       ])
       .then(() => res.send(`password updated`))
       .catch((error) => {
